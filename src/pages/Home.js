a8bf521f2ddcec28e0a11906cabc3732d3c88500
Hafid Mukhlasin <hafidmukhlasin@gmail.com>
2020-04-23 09:18:09 +0700

    test integration git+now

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ const Home = () => {
     <Layout>
       <div className="page">
         <h2>Halaman Home</h2>
+        source: https://github.com/hscstudio/react-smart-app
       </div>
     </Layout>
   </>
